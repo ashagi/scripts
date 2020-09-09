b931034f856ccbbc30df11531b10302c61a1f279
Askhat Shagirov <a.shagirov@gmail.com>
2020-09-09 09:33:27 -0700

    Upi add dest check and fix nohup to make no logs

--- upi.sh
+++ upi.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Global variables
+ZIP=$1
+DEST=~/Projects/sandbox
+
 # Check if arguments are supplied
 if [ $# -eq 0 ]; then
     echo "No arguments supplied"
@@ -7,21 +11,26 @@ if [ $# -eq 0 ]; then
 fi
 
 # Verify that passed file exists
-ZIP=$1
 if [ ! -f "$ZIP" ]; then
     echo "Passed file $ZIP does not exist"
     return 1
 fi
 
-echo "filename is "
-
-# Verify that target folder exists
-DEST=~/Projects/sandbox
+# Verify that target folder exist
 if [ ! -d "$DEST" ]; then
     echo "Target folder $DEST does not exist"
     return 1
 fi
 
+# Verify that project does not exist
+if [ -d "$DEST/${ZIP%%.*}" ]; then
+    printf "There is already a project at $DEST/${ZIP%%.*}\nPlease delete that project or change its name\n\n"
+    return 1
+fi
+
 echo "Unzipping "
 unzip $ZIP -d $DEST
-nohup idea "$DEST/${ZIP%%.*}" &
+
+echo "Opening idea with the project"
+# Explanation of nohup and pipes: https://stackoverflow.com/questions/10408816/how-do-i-use-the-nohup-command-without-getting-nohup-out
+nohup idea "$DEST/${ZIP%%.*}" > /dev/null 2>&1&
