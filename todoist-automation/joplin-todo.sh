548aa715535d4abaa4a09d0828c80119f5f3ba78
Askhat Shagirov <a.shagirov@gmail.com>
2021-09-06 12:29:52 -0700

    Create Joplin inbox todoist actions

---
+++ todoist-automation/joplin-todo.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# most likely not crossplatform but I run it in Windows only
+SCRIPT_PWD=$(cygpath -u $(dirname "$0"))
+
+source "$SCRIPT_PWD/secrets.sh"
+
+# switch internal field separator to newline from whitespace
+# otherwise FOR loops will iterate over whitespace of filenames
+IFS=$(echo -en "\n\r")
+
+INBOX_ITEMS=$(curl -s "http://localhost:41184/folders/$JOPLIN_INBOX_FOLDER_ID/notes?token=$JOPLIN_TOKEN" | jq -r ".items[].title")
+
+touch $SCRIPT_PWD/created_todos.txt
+[ -s $SCRIPT_PWD/created_todos.txt ] || echo "file must not be empty" >> $SCRIPT_PWD/created_todos.txt
+
+CREATED_TODOS=$(cat $SCRIPT_PWD/created_todos.txt)
+
+NEW_TODOS=$(echo "$INBOX_ITEMS" | grep -v "$CREATED_TODOS")
+
+for TODO in $NEW_TODOS
+do
+	curl "https://api.todoist.com/rest/v1/tasks" \
+    -X POST \
+    --data "{\"content\": \"Joplin inbox: $TODO\", \"project_id\": $PROJECT_ID, \"due_string\": \"Friday\", \"label_ids\": [$LABEL_ID]}" \
+    -H "Content-Type: application/json" \
+    -H "Authorization: Bearer $TODOIST_TOKEN"
+    echo $TODO >> $SCRIPT_PWD/created_todos.txt
+done
